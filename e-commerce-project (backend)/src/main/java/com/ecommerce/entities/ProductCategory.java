b55184e96a0bb831cc0bc9192b3b2eb70a1c4eea
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-10-18 23:35:07 +0200

    create product and productCateory entities

---
+++ e-commerce-project (backend)/src/main/java/com/ecommerce/entities/ProductCategory.java
@@ -0,0 +1,32 @@
+package com.ecommerce.entities;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+// @Data --known bug
+@Setter
+@Getter
+public class ProductCategory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "category_name")
+	private String categoryName ;
+	
+	private Set<Product> products;
+
+}
