b2d50690cca5687c439d91326a3b0ec74ba97387
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-11-05 19:07:12 +0100

    add to cart from details page

--- ecommerce-project (frontend)/src/app/componenets/product-details/product-details.component.ts
+++ ecommerce-project (frontend)/src/app/componenets/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/CartItem';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/Product.service';
 
 @Component({
@@ -9,16 +11,24 @@ import { ProductService } from 'src/app/services/Product.service';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+
   product : Product;
+  
 
   constructor(private productService : ProductService , 
-              private route:ActivatedRoute) { }
+              private route:ActivatedRoute , 
+              private catrtService : CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
     })
   }
+  addToCart() {
+    const theCartItem = new CartItem(this.product);
+    this.catrtService.addToCart(theCartItem);
+  }
+
 
 
   handleProductDetails() {
